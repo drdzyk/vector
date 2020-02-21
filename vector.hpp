42fe304138a47aa80618756378395b5fa42c388a
seismont <seismont@iponweb.net>
2020-02-21 12:02:56 +0300

    Combine logic of reserve() and reallocate_storage()

--- vector.hpp
+++ vector.hpp
@@ -23,26 +23,19 @@ namespace low
         void emplace_back(Args&& ...args)
         {
             alloc_type alloc;
-            if (size() == capacity())
-            {
-                reallocate_storage(alloc);
-            }
+            reallocate_storage_if_needed(alloc);
             alloc_traits::construct(alloc, end_, std::forward<Args>(args)...);
             end_ += 1u;
         }
 
-        void reserve(std::size_t count)
+        void reserve(std::size_t capacity_v)
         {
-            if (count <= capacity())
+            if (capacity_v <= capacity())
             {
                 return;
             }
             alloc_type alloc;
-            auto new_memory = alloc_traits::allocate(alloc, count);
-            release_storage(alloc);
-            begin_ = new_memory;
-            end_ = new_memory;
-            capacity_ = begin_ + count;
+            reallocate_storage(alloc, capacity_v, 0);
         }
 
         reference operator[](std::size_t idx)
@@ -80,23 +73,28 @@ namespace low
             alloc_traits::deallocate(alloc, begin_, capacity());
         }
 
-        void reallocate_storage(alloc_type &alloc)
+        void reallocate_storage(alloc_type &alloc, std::size_t capacity_v, std::size_t size_v)
         {
-            std::size_t current_size = size();
-            std::size_t next_capacity = get_next_capacity(current_size);
-
             // allocate new storage
-            auto new_memory = alloc_traits::allocate(alloc, next_capacity);
+            auto new_memory = alloc_traits::allocate(alloc, capacity_v);
             // move old data in new storage
-            std::uninitialized_move(begin_, end_, new_memory);
+            std::uninitialized_move(begin_, begin_ + size_v, new_memory);
             // freed old storage
             release_storage(alloc);
 
             begin_ = new_memory;
-            end_ = new_memory + current_size;
-            capacity_ = new_memory + next_capacity;
+            end_ = new_memory + size_v;
+            capacity_ = new_memory + capacity_v;
         }
-        static std::size_t get_next_capacity(std::size_t size) noexcept { return size ? size * 2 : 1; }
+
+        void reallocate_storage_if_needed(alloc_type &alloc)
+        {
+            if (auto current_size = size(); current_size == capacity())
+            {
+                std::size_t next_capacity = current_size ? current_size * 2 : 1;;
+                reallocate_storage(alloc, next_capacity, current_size);
+            }
+        }
 
         pointer begin_{nullptr};
         pointer end_{nullptr};
