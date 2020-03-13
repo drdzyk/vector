644504419459dbf272dfbdf1afb6fc4688061e3b
seismont <seismont@iponweb.net>
2020-03-13 13:14:39 +0300

    Add test for track allocations

--- test_vector_with_allocator.cpp
+++ test_vector_with_allocator.cpp
@@ -4,6 +4,7 @@
 struct GlobalTracker
 {
     std::size_t ctor{0}, copy_ctor{0}, move_ctor{0}, copy_assign{0}, move_assign{0}, dtor{0};
+    std::size_t alloc{0}, dealloc{0};
 
     bool operator==(const GlobalTracker &r) const noexcept
     {
@@ -12,7 +13,9 @@ struct GlobalTracker
                move_ctor == r.move_ctor &&
                copy_assign == r.copy_assign &&
                move_assign == r.move_assign &&
-               dtor == r.dtor;
+               dtor == r.dtor &&
+               alloc == r.alloc &&
+               dealloc == r.dealloc;
     }
 
     void clear() noexcept
@@ -23,12 +26,28 @@ struct GlobalTracker
         copy_assign = 0;
         move_assign = 0;
         dtor = 0;
+        alloc = 0;
+        dealloc = 0;
     }
 } global_tracker;
 
 template <typename T>
-struct tracked_allocator : std::allocator<T>
+struct tracked_allocator
 {
+    using value_type = T;
+
+    T *allocate(std::size_t n)
+    {
+        ++global_tracker.alloc;
+        return std::allocator<T>{}.allocate(n);
+    }
+
+    void deallocate(T *p, std::size_t s)
+    {
+        ++global_tracker.dealloc;
+        std::allocator<T>{}.deallocate(p, s);
+    }
+
     tracked_allocator() noexcept
     {
         ++global_tracker.ctor;
@@ -75,4 +94,22 @@ TEST_F(VectorWithAllocatorTest, ctor_with_allocator)
     low::vector<int, tracked_allocator<int>> v{alloc};
 
     ASSERT_EQ(global_tracker, (GlobalTracker{.ctor = 1, .copy_ctor = 1}));
+}
+
+TEST_F(VectorWithAllocatorTest, track_allocations)
+{
+    low::vector<int, tracked_allocator<int>> v;
+    ASSERT_EQ(global_tracker, (GlobalTracker{.ctor = 1}));
+    v.emplace_back(7);
+    ASSERT_EQ(global_tracker, (GlobalTracker{.ctor = 1, .alloc = 1, .dealloc = 1})); // TODO: there shouldn't be deallocation
+    v.emplace_back(8);
+    ASSERT_EQ(global_tracker, (GlobalTracker{.ctor = 1, .alloc = 2, .dealloc = 2}));
+    v.resize(0);
+    ASSERT_EQ(global_tracker, (GlobalTracker{.ctor = 1, .alloc = 2, .dealloc = 2}));
+    v.resize(2);
+    ASSERT_EQ(global_tracker, (GlobalTracker{.ctor = 1, .alloc = 2, .dealloc = 2}));
+    v.reserve(10);
+    ASSERT_EQ(global_tracker, (GlobalTracker{.ctor = 1, .alloc = 3, .dealloc = 3}));
+    v.clear(); // don't freed the storage;
+    ASSERT_EQ(global_tracker, (GlobalTracker{.ctor = 1, .alloc = 3, .dealloc = 3}));
 }
