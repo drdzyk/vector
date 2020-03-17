07d96ccc4fb0e842dd5df0671a8115a0436ac3e3
seismont <seismont@iponweb.net>
2020-03-17 12:00:59 +0300

    Move test of sizeof in separate file

--- test/test.cpp
+++ test/test.cpp
@@ -409,24 +409,3 @@ TYPED_TEST(VectorTest, empty_and_size)
     ASSERT_TRUE(v.empty());
     ASSERT_EQ(v.size(), 0);
 }
-
-TEST(VectorTest, sizeof_vector_with_stateless_allocator)
-{
-    // we employ EBO(empty base optimization) for stateless allocator
-    using alloc = std::allocator<int>;
-    static_assert(sizeof(alloc) == 1, "std::allocator is stateless");
-    static_assert(alignof(low::vector<int, alloc>) == sizeof(void *), "vector aligned by pointer size");
-    static_assert(sizeof(low::vector<int, alloc>) == 3 * sizeof(void *), "vector contains 3 pointer");
-}
-
-TEST(VectorTest, sizeof_vector_with_statefull_allocator)
-{
-    struct alloc : std::allocator<int>
-    {
-        int i{0};
-    };
-    static_assert(sizeof(alloc) == 4, "alloc contain state");
-    static_assert(alignof(low::vector<int, alloc>) == sizeof(void *), "vector aligned by pointer size");
-    static_assert(sizeof(low::vector<int, alloc>) == 3 * sizeof(void *)  + alignof(low::vector<int, alloc>),
-        "vector contains 3 pointer + aligned sizeof allocator");
-}
