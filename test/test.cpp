4ca0303cc7ea7664564bb3d43010832c89b09daa
seismont <seismont@iponweb.net>
2020-03-17 11:54:36 +0300

    Move DynamicInt class to separate file

--- test/test.cpp
+++ test/test.cpp
@@ -1,51 +1,7 @@
 #include <gtest/gtest.h>
 #include <vector>
 #include "vector.hpp"
-
-
-class DynamicInt
-{
-public:
-    DynamicInt() = default;
-    DynamicInt(int i) : i_(new int(i)) {}
-    DynamicInt(const DynamicInt &other) : i_(other.i_ ? new int(*other.i_) : nullptr)
-    {
-    }
-    DynamicInt(DynamicInt &&other): i_(other.i_)
-    {
-        other.i_ = nullptr;
-    }
-    DynamicInt& operator=(const DynamicInt &other)
-    {
-        delete i_;
-        i_ = new int(*other.i_);
-        return *this;
-    }
-    DynamicInt& operator=(DynamicInt &&other)
-    {
-        delete i_;
-        i_ = other.i_;
-        other.i_ = nullptr;
-        return *this;
-    }
-
-    ~DynamicInt() { delete i_; }
-
-    friend bool operator==(const DynamicInt &lhs, const DynamicInt &rhs) noexcept
-    {
-        if (rhs.i_ == nullptr && lhs.i_ == nullptr)
-        {
-            return true;
-        }
-        if (rhs.i_ == nullptr || lhs.i_ == nullptr)
-        {
-            return false;
-        }
-        return *lhs.i_ == *rhs.i_;
-    }
-private:
-    int *i_{nullptr};
-};
+#include "DynamicInt.hpp"
 
 using Types = testing::Types<int, std::uint32_t, float, double, DynamicInt>;
 TYPED_TEST_CASE(VectorTest, Types);
