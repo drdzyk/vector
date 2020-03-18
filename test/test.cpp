03b350f3d8180d66342e304542b8666211dc92e8
seismont <seismont@iponweb.net>
2020-03-18 10:51:31 +0300

    Rewrite test.cpp with Catch2

--- test/test.cpp
+++ test/test.cpp
@@ -1,391 +1,356 @@
-#include <gtest/gtest.h>
+#define CATCH_CONFIG_DISABLE_MATCHERS
+#define CATCH_CONFIG_FAST_COMPILE
+#define CATCH_CONFIG_COUNTER
+#include <catch2/catch.hpp>
 #include <vector>
 #include "vector.hpp"
 #include "DynamicInt.hpp"
 
-using Types = testing::Types<int, std::uint32_t, float, double, DynamicInt>;
-TYPED_TEST_CASE(VectorTest, Types);
-
-template <typename T>
-class VectorTest : public testing::Test
+TEMPLATE_PRODUCT_TEST_CASE("emplace_back", "[low::vector][std::vector]",
+    (low::vector, std::vector), (int, double, DynamicInt))
 {
-protected:
-};
-
-TYPED_TEST(VectorTest, emplace_back)
-{
-    low::vector<TypeParam> v;
-    ASSERT_EQ(v.size(), 0);
-    ASSERT_EQ(v.capacity(), 0);
+    TestType v;
+    REQUIRE(v.size() == 0);
+    REQUIRE(v.capacity() == 0);
 
     v.emplace_back(7);
-    ASSERT_EQ(v.size(), 1);
-    ASSERT_EQ(v.capacity(), 1);
-    ASSERT_EQ(v[0], 7);
+    REQUIRE(v.size() == 1);
+    REQUIRE(v.capacity() == 1);
+    REQUIRE(v[0] == 7);
 
     v.emplace_back(8);
-    ASSERT_EQ(v.size(), 2);
-    ASSERT_EQ(v.capacity(), 2);
-    ASSERT_EQ(v[0], 7);
-    ASSERT_EQ(v[1], 8);
+    REQUIRE(v.size() == 2);
+    REQUIRE(v.capacity() == 2);
+    REQUIRE(v[0] == 7);
+    REQUIRE(v[1] == 8);
 
     v.emplace_back(9);
-    ASSERT_EQ(v.size(), 3);
-    ASSERT_EQ(v.capacity(), 4);
-    ASSERT_EQ(v[0], 7);
-    ASSERT_EQ(v[1], 8);
-    ASSERT_EQ(v[2], 9);
+    REQUIRE(v.size() == 3);
+    REQUIRE(v.capacity() == 4);
+    REQUIRE(v[0] == 7);
+    REQUIRE(v[1] == 8);
+    REQUIRE(v[2] == 9);
 }
 
-TYPED_TEST(VectorTest, begin_end)
+TEMPLATE_PRODUCT_TEST_CASE("begin, end", "[low::vector][std::vector]",
+                           (low::vector, std::vector), (int, double, DynamicInt))
 {
-    low::vector<TypeParam> v;
-    ASSERT_EQ(v.begin(), v.end());
+    TestType v;
+    REQUIRE(v.begin() == v.end());
 
     v.emplace_back(7);
-    ASSERT_NE(v.begin(), v.end());
-    ASSERT_EQ(std::distance(v.begin(), v.end()), 1);
-    ASSERT_EQ(*v.begin(), 7);
+    REQUIRE(v.begin() != v.end());
+    REQUIRE(std::distance(v.begin(), v.end()) == 1);
+    REQUIRE(*v.begin() == 7);
 
     v.emplace_back(8);
-    ASSERT_NE(v.begin(), v.end());
-    ASSERT_EQ(std::distance(v.begin(), v.end()), 2);
-    ASSERT_EQ(*v.begin(), 7);
-    ASSERT_EQ(*std::next(v.begin()), 8);
+    REQUIRE(v.begin() != v.end());
+    REQUIRE(std::distance(v.begin(), v.end()) == 2);
+    REQUIRE(*v.begin() == 7);
+    REQUIRE(*std::next(v.begin()) == 8);
 
     *v.begin() = 9;
-    ASSERT_EQ(*v.begin(), 9);
-    ASSERT_EQ(*std::next(v.begin()), 8);
+    REQUIRE(*v.begin() == 9);
+    REQUIRE(*std::next(v.begin()) == 8);
 }
 
-TYPED_TEST(VectorTest, begin_end_const_overloads)
+TEMPLATE_PRODUCT_TEST_CASE("begin, end const overloads", "[low::vector][std::vector]",
+                           (low::vector, std::vector), (int, double, DynamicInt))
 {
-    const low::vector<TypeParam> v{7, 8, 9};
+    const TestType v{7, 8, 9};
 
-    ASSERT_NE(v.cbegin(), v.cend());
-    ASSERT_EQ(std::distance(v.cbegin(), v.cend()), 3);
-    ASSERT_EQ(*v.begin(), 7);
-    ASSERT_EQ(*std::next(v.begin()), 8);
-    ASSERT_EQ(*std::next(v.begin(), 2), 9);
+    REQUIRE(v.cbegin() != v.cend());
+    REQUIRE(std::distance(v.cbegin(), v.cend()) == 3);
+    REQUIRE(*v.begin() == 7);
+    REQUIRE(*std::next(v.begin()) == 8);
+    REQUIRE(*std::next(v.begin(), 2) == 9);
 }
 
-TYPED_TEST(VectorTest, cbegin_cend)
+TEMPLATE_PRODUCT_TEST_CASE("cbegin, cend", "[low::vector][std::vector]",
+                           (low::vector, std::vector), (int, double, DynamicInt))
 {
-    low::vector<TypeParam> v;
-    ASSERT_EQ(v.cbegin(), v.cend());
+    TestType v;
+    REQUIRE(v.cbegin() == v.cend());
 
     v.emplace_back(7);
-    ASSERT_NE(v.cbegin(), v.cend());
-    ASSERT_EQ(std::distance(v.cbegin(), v.cend()), 1);
-    ASSERT_EQ(*v.cbegin(), 7);
+    REQUIRE(v.cbegin() != v.cend());
+    REQUIRE(std::distance(v.cbegin(), v.cend()) == 1);
+    REQUIRE(*v.cbegin() == 7);
 
     v.emplace_back(8);
-    ASSERT_NE(v.cbegin(), v.cend());
-    ASSERT_EQ(std::distance(v.cbegin(), v.cend()), 2);
-    ASSERT_EQ(*v.cbegin(), 7);
-    ASSERT_EQ(*std::next(v.cbegin()), 8);
+    REQUIRE(v.cbegin() != v.cend());
+    REQUIRE(std::distance(v.cbegin(), v.cend()) == 2);
+    REQUIRE(*v.cbegin() == 7);
+    REQUIRE(*std::next(v.cbegin()) == 8);
 }
 
-TYPED_TEST(VectorTest, range_based_for)
+TEMPLATE_PRODUCT_TEST_CASE("range based for", "[low::vector][std::vector]",
+                           (low::vector, std::vector), (int, double, DynamicInt))
 {
-    low::vector<TypeParam> v{7, 8, 9};
+    TestType v{7, 8, 9};
 
     auto it = v.begin();
     for (const auto &e : v)
     {
-        ASSERT_EQ(e, *it++);
+        REQUIRE(e == *it++);
     }
 }
 
-template <typename Vector>
-static void test_reserve()
+TEMPLATE_PRODUCT_TEST_CASE("reserve", "[low::vector][std::vector]",
+                           (low::vector, std::vector), (int, double, DynamicInt))
 {
-    Vector v;
-    ASSERT_EQ(v.size(), 0);
-    ASSERT_EQ(v.capacity(), 0);
+    TestType v;
+    REQUIRE(v.size() == 0);
+    REQUIRE(v.capacity() == 0);
 
     v.reserve(1);
-    ASSERT_EQ(v.size(), 0);
-    ASSERT_EQ(v.capacity(), 1);
+    REQUIRE(v.size() == 0);
+    REQUIRE(v.capacity() == 1);
 
     v.emplace_back(77);
-    ASSERT_EQ(v.size(), 1);
-    ASSERT_EQ(v[0], 77);
-    ASSERT_EQ(v.capacity(), 1);
+    REQUIRE(v.size() == 1);
+    REQUIRE(v[0] == 77);
+    REQUIRE(v.capacity() == 1);
 
     v.reserve(11);
-    ASSERT_EQ(v.size(), 1);
-    ASSERT_EQ(v[0], 77);
-    ASSERT_EQ(v.capacity(), 11);
+    REQUIRE(v.size() == 1);
+    REQUIRE(v[0] == 77);
+    REQUIRE(v.capacity() == 11);
 
     v.reserve(11);
-    ASSERT_EQ(v.size(), 1);
-    ASSERT_EQ(v[0], 77);
-    ASSERT_EQ(v.capacity(), 11);
+    REQUIRE(v.size() == 1);
+    REQUIRE(v[0] == 77);
+    REQUIRE(v.capacity() == 11);
 
     v.reserve(7);
-    ASSERT_EQ(v.size(), 1);
-    ASSERT_EQ(v[0], 77);
-    ASSERT_EQ(v.capacity(), 11);
+    REQUIRE(v.size() == 1);
+    REQUIRE(v[0] == 77);
+    REQUIRE(v.capacity() == 11);
 
     for (std::size_t idx{0}; idx < 10; ++idx)
     {
         v.emplace_back(idx);
-        ASSERT_EQ(v.size(), idx + 2);
-        ASSERT_EQ(v.capacity(), 11);
+        REQUIRE(v.size() == idx + 2);
+        REQUIRE(v.capacity() == 11);
     }
 }
 
-TYPED_TEST(VectorTest, reserve)
+TEMPLATE_PRODUCT_TEST_CASE("resize", "[low::vector][std::vector]",
+                           (low::vector, std::vector), (int, double, DynamicInt))
 {
-    test_reserve<low::vector<TypeParam>>();
-}
+    TestType v;
+    using value_type =  typename TestType::value_type;
 
-TYPED_TEST(VectorTest, reserve_std_vector)
-{
-    test_reserve<std::vector<TypeParam>>();
-}
-
-template <typename Vector>
-static void test_resize()
-{
-    using value_type =  typename Vector::value_type;
-
-    Vector v;
     v.emplace_back(7);
     v.emplace_back(8);
     v.emplace_back(9);
 
-    ASSERT_EQ(v.size(), 3);
-    ASSERT_EQ(v.capacity(), 4);
-    ASSERT_EQ(v[0], 7);
-    ASSERT_EQ(v[1], 8);
-    ASSERT_EQ(v[2], 9);
+    REQUIRE(v.size() == 3);
+    REQUIRE(v.capacity() == 4);
+    REQUIRE(v[0] == 7);
+    REQUIRE(v[1] == 8);
+    REQUIRE(v[2] == 9);
 
     v.resize(1);
-    ASSERT_EQ(v.size(), 1);
-    ASSERT_EQ(v.capacity(), 4);
-    ASSERT_EQ(v[0], 7);
+    REQUIRE(v.size() == 1);
+    REQUIRE(v.capacity() == 4);
+    REQUIRE(v[0] == 7);
 
     v.resize(3);
-    ASSERT_EQ(v.size(), 3);
-    ASSERT_EQ(v.capacity(), 4);
-    ASSERT_EQ(v[0], 7);
-    ASSERT_EQ(v[1], value_type{});
-    ASSERT_EQ(v[2], value_type{});
+    REQUIRE(v.size() == 3);
+    REQUIRE(v.capacity() == 4);
+    REQUIRE(v[0] == 7);
+    REQUIRE(v[1] == value_type{});
+    REQUIRE(v[2] == value_type{});
 
     v.resize(4);
-    ASSERT_EQ(v.size(), 4);
-    ASSERT_EQ(v.capacity(), 4);
-    ASSERT_EQ(v[0], 7);
-    ASSERT_EQ(v[1], value_type{});
-    ASSERT_EQ(v[2], value_type{});
-    ASSERT_EQ(v[3], value_type{});
+    REQUIRE(v.size() == 4);
+    REQUIRE(v.capacity() == 4);
+    REQUIRE(v[0] == 7);
+    REQUIRE(v[1] == value_type{});
+    REQUIRE(v[2] == value_type{});
+    REQUIRE(v[3] == value_type{});
 
     v.resize(7);
-    ASSERT_EQ(v.size(), 7);
-    ASSERT_EQ(v.capacity(), 8);
-    ASSERT_EQ(v[0], 7);
-    ASSERT_EQ(v[1], value_type{});
-    ASSERT_EQ(v[2], value_type{});
-    ASSERT_EQ(v[3], value_type{});
-    ASSERT_EQ(v[4], value_type{});
-    ASSERT_EQ(v[5], value_type{});
-    ASSERT_EQ(v[6], value_type{});
+    REQUIRE(v.size() == 7);
+    REQUIRE(v.capacity() == 8);
+    REQUIRE(v[0] == 7);
+    REQUIRE(v[1] == value_type{});
+    REQUIRE(v[2] == value_type{});
+    REQUIRE(v[3] == value_type{});
+    REQUIRE(v[4] == value_type{});
+    REQUIRE(v[5] == value_type{});
+    REQUIRE(v[6] == value_type{});
 }
 
-template <typename Vector>
-static void test_valued_resize()
+TEMPLATE_PRODUCT_TEST_CASE("resize with value", "[low::vector][std::vector]",
+                           (low::vector, std::vector), (int, double, DynamicInt))
 {
-    using value_type =  typename Vector::value_type;
+    TestType v;
+    using value_type =  typename TestType::value_type;
 
-    Vector v;
     v.emplace_back(7);
     v.emplace_back(8);
     v.emplace_back(9);
 
-    ASSERT_EQ(v.size(), 3);
-    ASSERT_EQ(v.capacity(), 4);
-    ASSERT_EQ(v[0], 7);
-    ASSERT_EQ(v[1], 8);
-    ASSERT_EQ(v[2], 9);
+    REQUIRE(v.size() == 3);
+    REQUIRE(v.capacity() == 4);
+    REQUIRE(v[0] == 7);
+    REQUIRE(v[1] == 8);
+    REQUIRE(v[2] == 9);
 
     v.resize(1, value_type{777});
-    ASSERT_EQ(v.size(), 1);
-    ASSERT_EQ(v.capacity(), 4);
-    ASSERT_EQ(v[0], 7);
+    REQUIRE(v.size() == 1);
+    REQUIRE(v.capacity() == 4);
+    REQUIRE(v[0] == 7);
 
     v.resize(3, value_type{666});
-    ASSERT_EQ(v.size(), 3);
-    ASSERT_EQ(v.capacity(), 4);
-    ASSERT_EQ(v[0], 7);
-    ASSERT_EQ(v[1], value_type{666});
-    ASSERT_EQ(v[2], value_type{666});
+    REQUIRE(v.size() == 3);
+    REQUIRE(v.capacity() == 4);
+    REQUIRE(v[0] == 7);
+    REQUIRE(v[1] == value_type{666});
+    REQUIRE(v[2] == value_type{666});
 
     v.resize(4);
-    ASSERT_EQ(v.size(), 4);
-    ASSERT_EQ(v.capacity(), 4);
-    ASSERT_EQ(v[0], 7);
-    ASSERT_EQ(v[1], value_type{666});
-    ASSERT_EQ(v[2], value_type{666});
-    ASSERT_EQ(v[3], value_type{});
+    REQUIRE(v.size() == 4);
+    REQUIRE(v.capacity() == 4);
+    REQUIRE(v[0] == 7);
+    REQUIRE(v[1] == value_type{666});
+    REQUIRE(v[2] == value_type{666});
+    REQUIRE(v[3] == value_type{});
 
     v.resize(7, value_type{999});
-    ASSERT_EQ(v.size(), 7);
-    ASSERT_EQ(v.capacity(), 8);
-    ASSERT_EQ(v[0], 7);
-    ASSERT_EQ(v[1], value_type{666});
-    ASSERT_EQ(v[2], value_type{666});
-    ASSERT_EQ(v[3], value_type{});
-    ASSERT_EQ(v[4], value_type{999});
-    ASSERT_EQ(v[5], value_type{999});
-    ASSERT_EQ(v[6], value_type{999});
+    REQUIRE(v.size() == 7);
+    REQUIRE(v.capacity() == 8);
+    REQUIRE(v[0] == 7);
+    REQUIRE(v[1] == value_type{666});
+    REQUIRE(v[2] == value_type{666});
+    REQUIRE(v[3] == value_type{});
+    REQUIRE(v[4] == value_type{999});
+    REQUIRE(v[5] == value_type{999});
+    REQUIRE(v[6] == value_type{999});
 }
 
-TYPED_TEST(VectorTest, resize)
+TEMPLATE_PRODUCT_TEST_CASE("resize scale", "[low::vector][std::vector]",
+                           (low::vector, std::vector), (int, double, DynamicInt))
 {
-    test_resize<low::vector<TypeParam>>();
-    test_valued_resize<low::vector<TypeParam>>();
-}
-
-TYPED_TEST(VectorTest, resize_std_vector)
-{
-    test_resize<std::vector<TypeParam>>();
-    test_valued_resize<std::vector<TypeParam>>();
-}
-
-template <typename Vector>
-void test_resize_scale()
-{
-    using value_type =  typename Vector::value_type;
-    Vector v;
+    TestType v;
+    using value_type =  typename TestType::value_type;
 
     v.resize(8);
-    ASSERT_EQ(v.size(), 8);
-    ASSERT_EQ(v.capacity(), 8);
+    REQUIRE(v.size() == 8);
+    REQUIRE(v.capacity() == 8);
 
     v.resize(0);
-    ASSERT_EQ(v.size(), 0);
-    ASSERT_EQ(v.capacity(), 8);
+    REQUIRE(v.size() == 0);
+    REQUIRE(v.capacity() == 8);
 
     v.resize(7);
-    ASSERT_EQ(v.size(), 7);
-    ASSERT_EQ(v.capacity(), 8);
+    REQUIRE(v.size() == 7);
+    REQUIRE(v.capacity() == 8);
 
     v.resize(11);
-    ASSERT_EQ(v.size(), 11);
-    ASSERT_EQ(v.capacity(), 14);
+    REQUIRE(v.size() == 11);
+    REQUIRE(v.capacity() == 14);
 
     v.resize(23);
-    ASSERT_EQ(v.size(), 23);
-    ASSERT_EQ(v.capacity(), 23);
+    REQUIRE(v.size() == 23);
+    REQUIRE(v.capacity() == 23);
 }
 
-TYPED_TEST(VectorTest, resize_scale)
+TEMPLATE_PRODUCT_TEST_CASE("clear", "[low::vector][std::vector]",
+                           (low::vector, std::vector), (int, double, DynamicInt))
 {
-    test_resize_scale<low::vector<TypeParam>>();
-}
-
-TYPED_TEST(VectorTest, resize_scale_std_vector)
-{
-    test_resize_scale<std::vector<TypeParam>>();
-}
-
-template <typename Vector>
-void test_clear()
-{
-    Vector v;
+    TestType v;
 
     v.emplace_back(7);
     v.emplace_back(8);
     v.emplace_back(9);
-    ASSERT_EQ(v.size(), 3);
-    ASSERT_EQ(v.capacity(), 4);
+    REQUIRE(v.size() == 3);
+    REQUIRE(v.capacity() == 4);
 
     v.clear();
-    ASSERT_EQ(v.size(), 0);
-    ASSERT_EQ(v.capacity(), 4);
+    REQUIRE(v.size() == 0);
+    REQUIRE(v.capacity() == 4);
 
     v.clear(); // double clear() ok
-    ASSERT_EQ(v.size(), 0);
-    ASSERT_EQ(v.capacity(), 4);
+    REQUIRE(v.size() == 0);
+    REQUIRE(v.capacity() == 4);
 
     v.emplace_back(7);
     v.emplace_back(8);
-    ASSERT_EQ(v.size(), 2);
-    ASSERT_EQ(v.capacity(), 4);
-    ASSERT_EQ(v[0], 7);
-    ASSERT_EQ(v[1], 8);
+    REQUIRE(v.size() == 2);
+    REQUIRE(v.capacity() == 4);
+    REQUIRE(v[0] == 7);
+    REQUIRE(v[1] == 8);
 }
 
-TYPED_TEST(VectorTest, clear)
+TEMPLATE_PRODUCT_TEST_CASE("shrink_to_fit", "[low::vector][std::vector]",
+                           (low::vector, std::vector), (int, double, DynamicInt))
 {
-    test_clear<low::vector<TypeParam>>();
-}
+    TestType v;
 
-TYPED_TEST(VectorTest, clear_std_vector)
-{
-    test_clear<std::vector<TypeParam>>();
-}
-
-template <typename Vector>
-void test_shrink_to_fit()
-{
-    Vector v;
     v.shrink_to_fit();
     v.shrink_to_fit(); // double shrink_to_fit() is ok
 
     v.emplace_back(7);
     v.emplace_back(8);
     v.emplace_back(9);
-    ASSERT_EQ(v.size(), 3);
-    ASSERT_EQ(v.capacity(), 4);
+    REQUIRE(v.size() == 3);
+    REQUIRE(v.capacity() == 4);
 
     v.shrink_to_fit();
-    ASSERT_EQ(v.size(), 3);
-    ASSERT_EQ(v.capacity(), 3);
+    REQUIRE(v.size() == 3);
+    REQUIRE(v.capacity() == 3);
 
     v.shrink_to_fit(); // nothing changed
-    ASSERT_EQ(v.size(), 3);
-    ASSERT_EQ(v.capacity(), 3);
+    REQUIRE(v.size() == 3);
+    REQUIRE(v.capacity() == 3);
 
     v.clear();
-    ASSERT_EQ(v.size(), 0);
-    ASSERT_EQ(v.capacity(), 3);
+    REQUIRE(v.size() == 0);
+    REQUIRE(v.capacity() == 3);
 
     v.shrink_to_fit();
-    ASSERT_EQ(v.size(), 0);
-    ASSERT_EQ(v.capacity(), 0);
+    REQUIRE(v.size() == 0);
+    REQUIRE(v.capacity() == 0);
 
     v.shrink_to_fit(); // double shrink_to_fit() is ok
-    ASSERT_EQ(v.size(), 0);
-    ASSERT_EQ(v.capacity(), 0);
+    REQUIRE(v.size() == 0);
+    REQUIRE(v.capacity() == 0);
 }
 
-TYPED_TEST(VectorTest, shrink_to_fit)
+TEMPLATE_PRODUCT_TEST_CASE("empty, size", "[low::vector][std::vector]",
+                           (low::vector, std::vector), (int, double, DynamicInt))
 {
-    test_shrink_to_fit<low::vector<TypeParam>>();
-}
-TYPED_TEST(VectorTest, shrink_to_fit_std_vector)
-{
-    test_shrink_to_fit<std::vector<TypeParam>>();
+    TestType v;
+    REQUIRE(v.empty());
+    REQUIRE(v.size() == 0);
+
+    v.emplace_back(7);
+    REQUIRE_FALSE(v.empty());
+    REQUIRE(v.size() == 1);
+
+    v.clear();
+    REQUIRE(v.empty());
+    REQUIRE(v.size() == 0);
 }
 
 template <typename T>
 void assert_content_eq(const std::vector<T> &src, const low::vector<T> &v2)
 {
-    ASSERT_EQ(src.size(), v2.size());
-    ASSERT_EQ(src.capacity(), v2.capacity());
+    REQUIRE(src.size() == v2.size());
+    REQUIRE(src.capacity() == v2.capacity());
     for (std::size_t idx{0}; idx < src.size(); ++idx)
     {
-        ASSERT_EQ(src[idx], v2[idx]);
+        REQUIRE(src[idx] == v2[idx]);
     }
 }
 
-TYPED_TEST(VectorTest, emplace_back_regress)
+TEMPLATE_TEST_CASE("emplace_back scale", "[low::vector][std::vector]", int, double, DynamicInt)
 {
-    std::vector<TypeParam> v1;
-    low::vector<TypeParam> v2;
+    std::vector<TestType> v1;
+    low::vector<TestType> v2;
 
     for (std::size_t idx{0}; idx < 100; ++idx)
     {
@@ -393,19 +358,4 @@ TYPED_TEST(VectorTest, emplace_back_regress)
         v2.emplace_back(idx);
         assert_content_eq(v1, v2);
     }
-}
-
-TYPED_TEST(VectorTest, empty_and_size)
-{
-    low::vector<TypeParam> v;
-    ASSERT_TRUE(v.empty());
-    ASSERT_EQ(v.size(), 0);
-
-    v.emplace_back(7);
-    ASSERT_FALSE(v.empty());
-    ASSERT_EQ(v.size(), 1);
-
-    v.clear();
-    ASSERT_TRUE(v.empty());
-    ASSERT_EQ(v.size(), 0);
 }
