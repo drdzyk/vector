0d50f2657f2a12f74c2f8ea4e78bcb1358d6212c
seismont <seismont@iponweb.net>
2020-03-18 14:36:59 +0300

    Rewrite test_constructor.cpp with Catch2

--- test/test_constructor.cpp
+++ test/test_constructor.cpp
@@ -1,89 +1,94 @@
-#include <gtest/gtest.h>
+#define CATCH_CONFIG_DISABLE_MATCHERS
+#define CATCH_CONFIG_FAST_COMPILE
+#define CATCH_CONFIG_COUNTER
+#include <catch2/catch.hpp>
 #include <vector>
 #include "vector.hpp"
 #include "DynamicInt.hpp"
 
-using Types = testing::Types<int, std::uint32_t, float, double, DynamicInt>;
-TYPED_TEST_CASE(VectorTest, Types);
 
-template <typename T>
-class VectorTest : public testing::Test
+TEMPLATE_PRODUCT_TEST_CASE("vector move constructor", "[low::vector][std::vector]",
+                           (low::vector, std::vector), (int, double, DynamicInt))
 {
-protected:
-};
-
-TYPED_TEST(VectorTest, vector_move_constructor)
-{
-    low::vector<TypeParam> source;
+    TestType source;
     source.emplace_back(7);
     source.emplace_back(8);
-    ASSERT_EQ(source[0], 7);
-    ASSERT_EQ(source[1], 8);
+    REQUIRE(source[0] == 7);
+    REQUIRE(source[1] == 8);
 
     auto copy = std::move(source);
-    ASSERT_EQ(copy[0], 7);
-    ASSERT_EQ(copy[1], 8);
+    REQUIRE(copy[0] == 7);
+    REQUIRE(copy[1] == 8);
 }
 
-TYPED_TEST(VectorTest, vector_move_assignment_operator)
+TEMPLATE_PRODUCT_TEST_CASE("vector move assignment operator", "[low::vector][std::vector]",
+                           (low::vector, std::vector), (int, double, DynamicInt))
 {
-    low::vector<TypeParam> source;
+    TestType source;
     source.emplace_back(7);
     source.emplace_back(8);
-    ASSERT_EQ(source[0], 7);
-    ASSERT_EQ(source[1], 8);
+    REQUIRE(source[0] == 7);
+    REQUIRE(source[1] == 8);
 
-    low::vector<TypeParam> copy{1, 2, 3};
+    TestType copy{1, 2, 3};
     copy = std::move(source);
-    ASSERT_EQ(copy[0], 7);
-    ASSERT_EQ(copy[1], 8);
+    REQUIRE(copy[0] == 7);
+    REQUIRE(copy[1] == 8);
 
-    copy = std::move(copy); // check self assignment
-    ASSERT_EQ(copy[0], 7);
-    ASSERT_EQ(copy[1], 8);
+    using value_type = typename TestType::value_type;
+    if constexpr (std::is_same_v<TestType, low::vector<value_type>>)
+    {
+        // std::vector crashes in this case!
+        copy = std::move(copy); // check self assignment
+        REQUIRE(copy[0] == 7);
+        REQUIRE(copy[1] == 8);
+    }
 }
 
-TYPED_TEST(VectorTest, vector_copy_constructor)
+TEMPLATE_PRODUCT_TEST_CASE("vector copy constructor", "[low::vector][std::vector]",
+                           (low::vector, std::vector), (int, double, DynamicInt))
 {
-  low::vector<TypeParam> source;
-  source.emplace_back(7);
-  source.emplace_back(8);
-  ASSERT_EQ(source[0], 7);
-  ASSERT_EQ(source[1], 8);
+    TestType source;
+    source.emplace_back(7);
+    source.emplace_back(8);
+    REQUIRE(source[0] == 7);
+    REQUIRE(source[1] == 8);
 
-  auto copy = source;
-  ASSERT_EQ(copy[0], 7);
-  ASSERT_EQ(copy[1], 8);
+    auto copy = source;
+    REQUIRE(copy[0] == 7);
+    REQUIRE(copy[1] == 8);
 }
 
-TYPED_TEST(VectorTest, vector_initializer_list)
+TEMPLATE_PRODUCT_TEST_CASE("vector initializer_list constructor", "[low::vector][std::vector]",
+                           (low::vector, std::vector), (int, double, DynamicInt))
 {
+    using value_type = typename TestType::value_type;
     {
-        std::initializer_list<TypeParam> list;
-        low::vector<TypeParam> v{list};
-        ASSERT_TRUE(v.empty());
+        std::initializer_list<value_type> list;
+        TestType v{list};
+        REQUIRE(v.empty());
     }
     {
-        std::initializer_list<TypeParam> list{7};
-        low::vector<TypeParam> v{list};
-        ASSERT_EQ(v.size(), 1);
-        ASSERT_EQ(v.capacity(), 1);
-        ASSERT_EQ(v[0], 7);
+        std::initializer_list<value_type> list{7};
+        TestType v{list};
+        REQUIRE(v.size() == 1);
+        REQUIRE(v.capacity() == 1);
+        REQUIRE(v[0] == 7);
     }
     {
-        std::initializer_list<TypeParam> list{7, 8};
-        low::vector<TypeParam> v{list};
-        ASSERT_EQ(v.size(), 2);
-        ASSERT_EQ(v.capacity(), 2);
-        ASSERT_EQ(v[0], 7);
-        ASSERT_EQ(v[1], 8);
+        std::initializer_list<value_type> list{7, 8};
+        TestType v{list};
+        REQUIRE(v.size() == 2);
+        REQUIRE(v.capacity() == 2);
+        REQUIRE(v[0] == 7);
+        REQUIRE(v[1] == 8);
     }
     {
-        low::vector<TypeParam> v{1, 2, 3};
-        ASSERT_EQ(v.size(), 3);
-        ASSERT_EQ(v.capacity(), 3);
-        ASSERT_EQ(v[0], 1);
-        ASSERT_EQ(v[1], 2);
-        ASSERT_EQ(v[2], 3);
+        TestType v{1, 2, 3};
+        REQUIRE(v.size() == 3);
+        REQUIRE(v.capacity() == 3);
+        REQUIRE(v[0] == 1);
+        REQUIRE(v[1] == 2);
+        REQUIRE(v[2] == 3);
     }
 }
