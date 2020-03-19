29e673e0606642aaf978695d58daab615818076f
seismont <seismont@iponweb.net>
2020-03-19 17:44:01 +0300

    Move tests for iterators in test_iterators.cpp

---
+++ test/test_iterators.cpp
@@ -0,0 +1,71 @@
+#define CATCH_CONFIG_DISABLE_MATCHERS
+#define CATCH_CONFIG_FAST_COMPILE
+#define CATCH_CONFIG_COUNTER
+#include <catch2/catch.hpp>
+#include <vector>
+#include "vector.hpp"
+#include "DynamicInt.hpp"
+
+TEMPLATE_PRODUCT_TEST_CASE("begin, end", "[low::vector][std::vector]",
+                           (low::vector, std::vector), (int, double, DynamicInt))
+{
+    TestType v;
+    REQUIRE(v.begin() == v.end());
+
+    v.emplace_back(7);
+    REQUIRE(v.begin() != v.end());
+    REQUIRE(std::distance(v.begin(), v.end()) == 1);
+    REQUIRE(*v.begin() == 7);
+
+    v.emplace_back(8);
+    REQUIRE(v.begin() != v.end());
+    REQUIRE(std::distance(v.begin(), v.end()) == 2);
+    REQUIRE(*v.begin() == 7);
+    REQUIRE(*std::next(v.begin()) == 8);
+
+    *v.begin() = 9;
+    REQUIRE(*v.begin() == 9);
+    REQUIRE(*std::next(v.begin()) == 8);
+}
+
+TEMPLATE_PRODUCT_TEST_CASE("begin, end const overloads", "[low::vector][std::vector]",
+                           (low::vector, std::vector), (int, double, DynamicInt))
+{
+    const TestType v{7, 8, 9};
+
+    REQUIRE(v.cbegin() != v.cend());
+    REQUIRE(std::distance(v.cbegin(), v.cend()) == 3);
+    REQUIRE(*v.begin() == 7);
+    REQUIRE(*std::next(v.begin()) == 8);
+    REQUIRE(*std::next(v.begin(), 2) == 9);
+}
+
+TEMPLATE_PRODUCT_TEST_CASE("cbegin, cend", "[low::vector][std::vector]",
+                           (low::vector, std::vector), (int, double, DynamicInt))
+{
+    TestType v;
+    REQUIRE(v.cbegin() == v.cend());
+
+    v.emplace_back(7);
+    REQUIRE(v.cbegin() != v.cend());
+    REQUIRE(std::distance(v.cbegin(), v.cend()) == 1);
+    REQUIRE(*v.cbegin() == 7);
+
+    v.emplace_back(8);
+    REQUIRE(v.cbegin() != v.cend());
+    REQUIRE(std::distance(v.cbegin(), v.cend()) == 2);
+    REQUIRE(*v.cbegin() == 7);
+    REQUIRE(*std::next(v.cbegin()) == 8);
+}
+
+TEMPLATE_PRODUCT_TEST_CASE("range based for", "[low::vector][std::vector]",
+                           (low::vector, std::vector), (int, double, DynamicInt))
+{
+    TestType v{7, 8, 9};
+
+    auto it = v.begin();
+    for (const auto &e : v)
+    {
+        REQUIRE(e == *it++);
+    }
+}
