ea129e9f25891047797c11ab891832da7aaaa07d
seismont <seismont@iponweb.net>
2020-03-24 13:48:02 +0300

    Introduce ComparableAllocator class

--- test/test_move_constructor.cpp
+++ test/test_move_constructor.cpp
@@ -32,13 +32,24 @@ TEMPLATE_PRODUCT_TEST_CASE("move constructor", "[low::vector][std::vector]",
 namespace
 {
     template <typename T>
-    struct NotEqualAllocator
+    struct ComparableAllocator
     {
         using value_type = T;
 
         T *allocate(std::size_t n) { return std::allocator<T>{}.allocate(n); }
         void deallocate(T *p, std::size_t s) { std::allocator<T>{}.deallocate(p, s); }
+    };
 
+    template <typename T>
+    struct EqualAllocator : ComparableAllocator<T>
+    {
+        friend bool operator==(const EqualAllocator&, const EqualAllocator&) noexcept { return true; }
+        friend bool operator!=(const EqualAllocator&l, const EqualAllocator&r) noexcept { return !(l == r); }
+    };
+
+    template <typename T>
+    struct NotEqualAllocator : ComparableAllocator<T>
+    {
         friend bool operator==(const NotEqualAllocator&, const NotEqualAllocator&) noexcept { return false; }
         friend bool operator!=(const NotEqualAllocator&l, const NotEqualAllocator&r) noexcept { return !(l == r); }
     };
