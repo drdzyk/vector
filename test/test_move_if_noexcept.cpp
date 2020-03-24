fe048abe31a569293b2e34680e7ff65b1cb60d4b
seismont <seismont@iponweb.net>
2020-03-24 13:37:50 +0300

    Renamings

--- test/test_move_if_noexcept.cpp
+++ test/test_move_if_noexcept.cpp
@@ -5,11 +5,11 @@
 #include <vector>
 #include "vector.hpp"
 
-struct Tracker
+struct TypeTracker
 {
     std::size_t ctor{0}, copy_ctor{0}, move_ctor{0}, copy_assign{0}, move_assign{0}, dtor{0};
 
-    bool operator==(const Tracker&r) const noexcept
+    bool operator==(const TypeTracker&r) const noexcept
     {
         return ctor == r.ctor &&
             copy_ctor == r.copy_ctor &&
@@ -27,93 +27,93 @@ struct NoexceptCopyAssignTag{};
 struct NoexceptMoveAssignTag{};
 
 template <typename ...Tags>
-class A
+class TrackedType
 {
 public:
-    A(const std::shared_ptr<Tracker> &tracker) noexcept((std::is_same_v<NoexceptCtorTag, Tags> || ...))
+    TrackedType(const std::shared_ptr<TypeTracker> &tracker) noexcept((std::is_same_v<NoexceptCtorTag, Tags> || ...))
       : tracker_(tracker)
     {
         ++tracker_->ctor;
     }
-    A(const A &other) noexcept((std::is_same_v<NoexceptCopyCtorTag, Tags> || ...))
+    TrackedType(const TrackedType &other) noexcept((std::is_same_v<NoexceptCopyCtorTag, Tags> || ...))
       : tracker_(other.tracker_)
     {
         ++tracker_->copy_ctor;
     }
-    A(A &&other) noexcept((std::is_same_v<NoexceptMoveCtorTag, Tags> || ...))
+    TrackedType(TrackedType &&other) noexcept((std::is_same_v<NoexceptMoveCtorTag, Tags> || ...))
       : tracker_(other.tracker_)
     {
         ++tracker_->move_ctor;
     }
-    A& operator=(const A &other) noexcept((std::is_same_v<NoexceptCopyAssignTag, Tags> || ...))
+    TrackedType& operator=(const TrackedType &other) noexcept((std::is_same_v<NoexceptCopyAssignTag, Tags> || ...))
     {
         ++tracker_->copy_assign;
         return *this;
     }
-    A& operator=(A &&other) noexcept((std::is_same_v<NoexceptMoveAssignTag, Tags> || ...))
+    TrackedType& operator=(TrackedType &&other) noexcept((std::is_same_v<NoexceptMoveAssignTag, Tags> || ...))
     {
         ++tracker_->move_assign;
         return *this;
     }
     // should be always noexcept, otherwise std::is_nothrow_move_constructible_v will return false,
     // whatever move ctor noexcept specifier
-    ~A() noexcept
+    ~TrackedType() noexcept
     {
         ++tracker_->dtor;
     }
 
 private:
-    const std::shared_ptr<Tracker> tracker_;
+    const std::shared_ptr<TypeTracker> tracker_;
 };
 
 TEMPLATE_PRODUCT_TEST_CASE("reserve on throwing move ctor", "[low::vector][std::vector]",
-                           (low::vector, std::vector), (A<>))
+                           (low::vector, std::vector), (TrackedType<>))
 {
-    const auto tracker = std::make_shared<Tracker>();
+    const auto tracker = std::make_shared<TypeTracker>();
     TestType v;
 
     v.emplace_back(tracker);
-    REQUIRE(*tracker == (Tracker{.ctor = 1}));
+    REQUIRE(*tracker == (TypeTracker{.ctor = 1}));
 
     v.reserve(2);
-    REQUIRE(*tracker == (Tracker{.ctor = 1, .copy_ctor = 1, .dtor = 1}));
+    REQUIRE(*tracker == (TypeTracker{.ctor = 1, .copy_ctor = 1, .dtor = 1}));
 }
 
 TEMPLATE_PRODUCT_TEST_CASE("reserve on noexcept move ctor", "[low::vector][std::vector]",
-                           (low::vector, std::vector), (A<NoexceptMoveCtorTag>))
+                           (low::vector, std::vector), (TrackedType<NoexceptMoveCtorTag>))
 {
-    const auto tracker = std::make_shared<Tracker>();
+    const auto tracker = std::make_shared<TypeTracker>();
     TestType v;
 
     v.emplace_back(tracker);
-    REQUIRE(*tracker ==(Tracker{.ctor = 1}));
+    REQUIRE(*tracker ==(TypeTracker{.ctor = 1}));
 
     v.reserve(2);
-    REQUIRE(*tracker == (Tracker{.ctor = 1, .move_ctor = 1, .dtor = 1}));
+    REQUIRE(*tracker == (TypeTracker{.ctor = 1, .move_ctor = 1, .dtor = 1}));
 }
 
 TEMPLATE_PRODUCT_TEST_CASE("resize on throwing move ctor", "[low::vector][std::vector]",
-                           (low::vector, std::vector), (A<>))
+                           (low::vector, std::vector), (TrackedType<>))
 {
-    const auto tracker = std::make_shared<Tracker>();
-    low::vector<A<>> v;
+    const auto tracker = std::make_shared<TypeTracker>();
+    low::vector<TrackedType<>> v;
 
     v.emplace_back(tracker);
-    REQUIRE(*tracker == (Tracker{.ctor = 1}));
+    REQUIRE(*tracker == (TypeTracker{.ctor = 1}));
 
     v.resize(2, tracker);
-    REQUIRE(*tracker == (Tracker{.ctor = 2, .copy_ctor = 2, .dtor = 2}));
+    REQUIRE(*tracker == (TypeTracker{.ctor = 2, .copy_ctor = 2, .dtor = 2}));
 }
 
 TEMPLATE_PRODUCT_TEST_CASE("resize on noexcept move ctor", "[low::vector][std::vector]",
-                           (low::vector, std::vector), (A<NoexceptMoveCtorTag>))
+                           (low::vector, std::vector), (TrackedType<NoexceptMoveCtorTag>))
 {
-    const auto tracker = std::make_shared<Tracker>();
-    low::vector<A<NoexceptMoveCtorTag>> v;
+    const auto tracker = std::make_shared<TypeTracker>();
+    low::vector<TrackedType<NoexceptMoveCtorTag>> v;
 
     v.emplace_back(tracker);
-    REQUIRE(*tracker == (Tracker{.ctor = 1}));
+    REQUIRE(*tracker == (TypeTracker{.ctor = 1}));
 
     v.resize(2, tracker);
-    REQUIRE(*tracker == (Tracker{.ctor = 2, .copy_ctor = 1, .move_ctor = 1, .dtor = 2}));
+    REQUIRE(*tracker == (TypeTracker{.ctor = 2, .copy_ctor = 1, .move_ctor = 1, .dtor = 2}));
 }
