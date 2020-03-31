58a00a0048748fc780cf810de2437006215fe0ed
seismont <seismont@iponweb.net>
2020-03-31 12:06:11 +0300

    Extend test

--- test/test_assign_tracker.cpp
+++ test/test_assign_tracker.cpp
@@ -34,10 +34,57 @@ TEMPLATE_PRODUCT_TEST_CASE("assign tracker with move iterators", "[low::vector][
         TestType v;
         v.reserve(2);
         v.emplace_back(tracker);
+        REQUIRE(*tracker == (TypeTracker{.ctor = 3}));
+
+        v.assign(std::move_iterator{source.begin()}, std::move_iterator{source.end()});
+        REQUIRE(*tracker == (TypeTracker{.ctor = 3, .move_ctor = 1, .move_assign = 1}));
+    }
+    SECTION("move to not empty vector with enough capacity")
+    {
+        TestType v;
+        v.reserve(2);
+        v.emplace_back(tracker);
         v.emplace_back(tracker);
         REQUIRE(*tracker == (TypeTracker{.ctor = 4}));
 
         v.assign(std::move_iterator{source.begin()}, std::move_iterator{source.end()});
         REQUIRE(*tracker == (TypeTracker{.ctor = 4, .move_assign = 2}));
+    }
+    SECTION("move to not empty vector with enough capacity")
+    {
+        TestType v;
+        v.reserve(3);
+        v.emplace_back(tracker);
+        v.emplace_back(tracker);
+        REQUIRE(*tracker == (TypeTracker{.ctor = 4}));
+
+        v.assign(std::move_iterator{source.begin()}, std::move_iterator{source.end()});
+        REQUIRE(*tracker == (TypeTracker{.ctor = 4, .move_assign = 2}));
+    }
+    SECTION("move to not empty vector with enough capacity")
+    {
+        TestType v;
+        v.reserve(3);
+        v.emplace_back(tracker);
+        v.emplace_back(tracker);
+        v.emplace_back(tracker);
+        REQUIRE(*tracker == (TypeTracker{.ctor = 5}));
+
+        v.assign(std::move_iterator{source.begin()}, std::move_iterator{source.end()});
+        REQUIRE(*tracker == (TypeTracker{.ctor = 5, .move_assign = 2, .dtor = 1}));
+    }
+    SECTION("move to not empty vector with enough capacity")
+    {
+        TestType v;
+        v.reserve(5);
+        v.emplace_back(tracker);
+        v.emplace_back(tracker);
+        v.emplace_back(tracker);
+        v.emplace_back(tracker);
+        v.emplace_back(tracker);
+        REQUIRE(*tracker == (TypeTracker{.ctor = 7}));
+
+        v.assign(std::move_iterator{source.begin()}, std::move_iterator{source.end()});
+        REQUIRE(*tracker == (TypeTracker{.ctor = 7, .move_assign = 2, .dtor = 3}));
     }
 }
